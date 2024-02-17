428328ead1ca2ff2d239b4b1c8e6dc617ec5fa39
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-02-18 00:32:08 +0100

    Update 1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,16 @@
 export const App = () => {
-  console.log('React');
-  return <h1>React</h1>;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
