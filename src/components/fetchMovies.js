cba0f797f260a0f3b0b22a15097c01fa0ad85356
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-02-19 11:23:34 +0100

    fix/small fixes

--- src/components/fetchMovies.js
+++ src/components/fetchMovies.js
@@ -7,9 +7,10 @@ const apiKey = 'e58ffa44180a16235283b8c62dada0cb';
 export const fetchMovies = async movieId => {
   try {
     const response = await axios.get(`/3/movie/${movieId}/?api_key=${apiKey}`);
-    console.log('bad_bob');
+
     console.log(response);
   } catch (error) {
+    console.log('bad_bob');
     console.log('Failed fetch data: ', error);
   }
 };
