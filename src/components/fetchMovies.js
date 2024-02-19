77953007256af598322887de76f57e3529ca2a5c
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-02-19 11:29:44 +0100

    fix/checking conection

--- src/components/fetchMovies.js
+++ src/components/fetchMovies.js
@@ -6,7 +6,7 @@ const apiKey = 'e58ffa44180a16235283b8c62dada0cb';
 
 export const fetchMovies = async movieId => {
   try {
-    const response = await axios.get(`/3/movie/${movieId}/?api_key=${apiKey}`);
+    const response = await axios.get(`/3/movie/?api_key=${apiKey}`);
 
     console.log(response);
   } catch (error) {
