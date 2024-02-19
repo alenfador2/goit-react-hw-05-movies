5eb2d66d8e90ebc96740942f8831dc42c708b419
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-02-19 11:06:25 +0100

    Update 1.6 feature/ add homepage & components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { fetchMovies } from './fetchMovies';
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MoviesDetails = lazy(() =>
+  import('../pages/MoviesDetails/MoviesDetails')
+);
+
 export const App = () => {
+  fetchMovies(157336);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}>
+          <Route path=":movieId" element={<MoviesDetails />}></Route>
+        </Route>
+      </Routes>
+    </>
   );
 };
---
+++ src/components/fetchMovies.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://www.api.themoviedb.org';
+
+const apiKey = 'e58ffa44180a16235283b8c62dada0cb';
+
+export const fetchMovies = async movieId => {
+  try {
+    const response = await axios.get(`/3/movie/${movieId}/?api_key=${apiKey}`);
+    console.log(response);
+  } catch (error) {
+    console.log('Failed fetch data: ', error);
+  }
+};
